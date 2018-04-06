ad014b1a2147b49d06b270034176b28948e30ae4
Daniel Reed <daniel.reed998@gmail.com>
2018-04-06 05:41:25 -0500

    final routing changes

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 
 router.post('/name', (req, res, next) => {
     const name = req.body.name;
-    console.log('hitting backend?');
     if (name != null) {
         res.json(`Hello, ${name}`);
     } else {
